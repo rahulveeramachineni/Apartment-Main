fdfce46e8255fdd702d5c540d5dcedf2198537f9
Rahul <rahulvneni@gmail.com>
2025-03-16 13:28:24 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
-    mongoURI: "mongodb+srv://rahulvneni:Rahul%405643@apartment-main.xvryp.mongodb.net/YOUR_DATABASE_NAME?retryWrites=true&w=majority&appName=Apartment-Main",
+    mongoURI: "mongodb+srv://rahulvneni:<db_password>@apartment-main.xvryp.mongodb.net/?retryWrites=true&w=majority&appName=Apartment-Main
+
+",
     secretOrKey: "secret"
 };
