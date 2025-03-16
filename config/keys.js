4bac603714b32d07da7b478471642be673b2a775
rahulveeramachineni <rahulvneni@gmail.com>
2025-03-16 13:36:12 +0530

    small changes

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb+srv://rahulvneni:<db_password>@apartment-main.xvryp.mongodb.net/?retryWrites=true&w=majority&appName=Apartment-Main
-
-",
-    secretOrKey: "secret"
+  mongoURI:
+    "mongodb+srv://rahulvneni:Rahul%405643@apartment-main.xvryp.mongodb.net/YOUR_DATABASE_NAME?retryWrites=true&w=majority&appName=Apartment-Main",
+  secretOrKey: "secret",
 };
